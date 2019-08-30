44020faeaf71a1b3cf409e5641d8bf89d46f508f
Divya Bhulai <37069270+DivyaBhulai@users.noreply.github.com>
2019-08-30 13:08:37 +0200

    Update key-komt-voor.py
    key komt voor is klaar

--- key-komt-voor.py
+++ key-komt-voor.py
@@ -1,2 +1,9 @@
-fruit = {0: "meloen", 1: "aardbei", 2: "appel"}
+fruit = {"0": "meloen", "1": "aardbei", "2": "appel"}
 #print(fruit)
+print("Naar welke key zoek je? Key ")
+x = input()
+if x in fruit:
+    print("Key komt voor in de dictionarie fruit.")
+else:
+    print("Key komt niet voor in dictionarie fruit")
+    print(fruit)
