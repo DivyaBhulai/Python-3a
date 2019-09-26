26814d5e54ca1a47a4ce0db3a2ea48023fe55fd4
Divya Bhulai <37069270+DivyaBhulai@users.noreply.github.com>
2019-09-26 10:39:40 +0200

    eindopdracht

--- boodschappen.py
+++ boodschappen.py
@@ -1,32 +1,28 @@
 producten = {"brood": "2", "sap": "3", "pizza": "5", "melk": "2", "mayonaise": "1"}
 totaal = 0
 import datetime
-
+'''
 def keuze():
     kiezen = input("Wil je het keuzemenu zien? Kies ja of nee ")
     if(kiezen == "ja"):
-        keuzemenu()
+        
     if(kiezen == "nee"):
         exit()
     else:
         print("Je hebt iets anders dan ja of nee ingevuld")
-        keuzemenu()
-
+'''
 def optie_1(p):
     print("Zie rechts de producten met daarnaast hun prijs.")
     print(p)
-    keuzemenu()
     
 def optie_2():
     product = input("Typ het product wat je wilt toevoegen. ")
     if(product in producten):
         print("Dit product staat al in de lijst.")
-        keuzemenu()
     prijs = input("Typ de prijs van het product ")
     producten[product] = prijs
     print("Het product is toegevoegd.")
     print(producten)
-    keuzemenu()
     return producten
     
 def optie_3():
@@ -34,9 +30,7 @@ def optie_3():
     if(product_2 in producten):
         del producten[product_2]
         print(producten)
-        keuzemenu()
     print("Dit product staat niet in de lijst.")
-    keuzemenu()
     
 def optie_4():
     product_3 = input("Typ het product wat je wilt wijzigen. ")
@@ -45,11 +39,9 @@ def optie_4():
         producten[product_3] = prijs_3
         print("Het product is gewijzigd.")
         print(producten)
-        keuzemenu()
     else:
         print("Het product staat niet in de lijst.")
         print("Kies optie 2 om het product toe te voegen.")
-        keuzemenu()
         
 def optie_5(p):
     print(p)
@@ -67,11 +59,11 @@ def optie_5(p):
             else:
                 print("Dit product staat niet in de lijst.")
                 print("Kies optie 2 om het product toe te voegen.")
-                return totaal + keuzemenu()
+                return totaal
         else:
-            return totaal + keuzemenu()
+            return totaal
     print("Je moet €", totaal, "betalen")
-    return totaal + keuzemenu()
+    return totaal
     #print("in functie :" + totaal)
     #return totaal
     
@@ -89,42 +81,31 @@ def optie_6(p):
     f.close()
     f = open("save.txt", "r")
     print(f.read())
-    keuzemenu()
     return
 
-def keuzemenu():
-    invoer = input("Wil je het keuzemenu zien? ")
-    while invoer == "ja":
-    #global totaal
-        print("1. Bekijk alle producten.")
-        print("2. Voeg een product toe.")
-        print("3. Verwijder een product.")
-        print("4. Wijzig een product.")
-        print("5. Boodschappen doen.")
-        print("6. Sla producten op.")
-        kies = input("Kies een optie ")
-        
-        if(kies == "1"):
-            optie_1(producten)
-            return kies == "1"
-        if(kies == "2"):
-            optie_2()
-            return kies == "2"
-        if(kies == "3"):
-            optie_3()
-            return kies == "3"
-        if(kies == "4"):
-            optie_4()
-            return kies == "4"
-        if(kies == "5"):
-            totaal = optie_5(producten)
-            return kies == "5"
-        if(kies == "6"):
-            #print(totaal)
-            optie_6(producten)
-            return kies == "6"
-        else:
-            print("Je hebt iets verkeerds ingevuld.")
-            keuze()
-             
-keuzemenu()
+invoer = input("Wil je het keuzemenu zien? ")
+while invoer == "ja":
+#global totaal
+    print("1. Bekijk alle producten.")
+    print("2. Voeg een product toe.")
+    print("3. Verwijder een product.")
+    print("4. Wijzig een product.")
+    print("5. Boodschappen doen.")
+    print("6. Sla producten op.")
+    kies = input("Kies een optie ")
+    
+    if(kies == "1"):
+        optie_1(producten)
+    if(kies == "2"):
+        optie_2()
+    if(kies == "3"):
+        optie_3()
+    if(kies == "4"):
+        optie_4()
+    if(kies == "5"):
+        totaal = optie_5(producten)
+    if(kies == "6"):
+        #print(totaal)
+        optie_6(producten)
+    #else:
+        #print("Je hebt iets verkeerds ingevuld.")
